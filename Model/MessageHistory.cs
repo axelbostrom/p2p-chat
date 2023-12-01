1a603df97c4cc28842284e97a05f678803772b6c
axelbostrom <axel.bostrom103@gmail.com>
2023-12-01 11:26:49 +0100

    added messageHistory

---
+++ Model/MessageHistory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ChatApp.Model
+{
+    public class MessageHistory
+    {
+        private string _userName;
+        private ObservableCollection<Message> _messages;
+
+        public MessageHistory(string userName, ObservableCollection<Message> messages)
+        {
+            _userName = userName;
+            _messages = messages;
+        }
+
+        public string UserName { get { return _userName; } }
+
+        public ObservableCollection<Message> Messages { get { return _messages; } }
+
+        public void AddMessage(Message message)
+        {
+            _messages.Add(message);
+        }
+    }
+}
